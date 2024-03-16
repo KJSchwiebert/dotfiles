50cffbeb95e2a9f03de7313df0f31e55103ae1a0
igloo-party <kjschwie@gmail.com>
2024-03-16 12:37:50 -0400

    Used a much lighter install directory that sets up static links instead of obliterating ~/.vim. Also updated some settings for CoC.

--- install.sh
+++ install.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 # Install the files from the git repo to the particular machine.
-rm ~/.vim -d -r -f
-cp ./.vim ~/.vim -d -r
+rm ~/.vim/vimrc
+ln ./.vim/vimrc ~/.vim/vimrc
+mkdir ~/.vim/autoload
+rm ~/.vim/autoload/plug.vim
+ln ./.vim/autoload/plug.vim ~/.vim/autoload/plug.vim
 vim +PlugInstall +PlugClean +qall
