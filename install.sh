259340d6a0407baf7460b82602c69c6f0da2c7b2
Kyle Schwiebert <kjschwie@gmail.com>
2024-04-11 20:04:02 -0400

    Added bash and git configureation files. Added sketch of support for latex with ultisnips. Updated install script to make symlinks instead of hard links.

--- install.sh
+++ install.sh
@@ -3,16 +3,26 @@
 # Assume that you have vim install as well as the dependancies of all packages.
 # For example CoC requires that you have nodejs.
 rm ~/.vim/vimrc
-mkdir ~/.vim
-ln ./.vim/vimrc ~/.vim/vimrc
-mkdir ~/.vim/autoload
+mkdir -p ~/.vim
+ln -s ~/dotfiles/.vim/vimrc ~/.vim/vimrc
+mkdir -p ~/.vim/autoload
+rm ~/.vim/UltiSnips
+ln -s ~/dotfiles/.vim/UltiSnips ~/.vim/UltiSnips
 rm ~/.vim/autoload/plug.vim
-ln ./.vim/autoload/plug.vim ~/.vim/autoload/plug.vim
+ln -s ~/dotfiles/.vim/autoload/plug.vim ~/.vim/autoload/plug.vim
 vim +PlugInstall +PlugClean +qall
 
 # Get gdb settings.
 rm ~/.gdbinit
-ln ./.gdbinit ~/.gdbinit
+ln -s ~/dotfiles/.gdbinit ~/.gdbinit
+
+# Get git configuration.
+rm ~/.gitconfig
+ln -s ~/dotfiles/.gitconfig ~/.gitconfig
+
+# Get bash configuration.
+rm ~/.bashrc
+ln -s ~/dotfiles/.bashrc ~/.bashrc
 
 # This is required to make sure we get syntax highlighting and stuff for CoC.
 # To setup CoC for cpp you will need to do a few other things including running
@@ -20,4 +30,4 @@ ln ./.gdbinit ~/.gdbinit
 # nodejs and perhaps clang compiler. These are all available with apt,
 # google the commands.
 rm ~/.vim/coc-settings.json
-ln ./.vim/coc-settings.json ~/.vim/coc-settings.json
+ln -s ~/dotfiles/.vim/coc-settings.json ~/.vim/coc-settings.json
